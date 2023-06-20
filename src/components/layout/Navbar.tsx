b4f5f501dfdb7f620a15a858b474eef2e2a7ac57
rurigrass <ruripg@gmail.com>
2023-06-20 14:14:22 +0200

    schema fixes capitals

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import { Icons } from "./Icons";
 import { buttonVariants } from "../ui/Button";
-import { getAuthSession } from "@/lib/auth";
+import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import UserAccountNav from "./UserAccountNav";
 
 const Navbar = async () => {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions)
 
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
